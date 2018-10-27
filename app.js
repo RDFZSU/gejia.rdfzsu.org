277b0ec2062bab8598a955eeccde5f6e17c39f52
zhjimin <44405840+zhjimin@users.noreply.github.com>
2018-10-27 18:21:39 +0800

    Oct.27 18:21
    Oct.27 18:21

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+//ads by Alan-Liang
+console.log("%c想参与学生会后续前端&后端制作吗？\n加入我们吧！我的邮箱:liangyalun [at] rdfzsu [dot] org\n技术支持 Alan-Liang zhjimin\n","color:#f5fafd;background-color:#002d4d;font-size:32px;");
+
 var $=mdui.JQ;
 var topNav=$("#top-nav");
 topNav.find("a").each((i,el) => {
