7955e641277c32f06309d25f2f1d8da5ac0b860b
Alan?Liang <a@keeer.net>
2018-10-28 14:12:04 +0800

    Update 18/10/28
    Add test
    Fix background
    Remove useless links
    Alter logic of nav bar

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 //ads by Alan-Liang
 console.log("%c想参与学生会后续前端&后端制作吗？\n加入我们吧！我的邮箱:liangyalun [at] rdfzsu [dot] org\n技术支持 Alan-Liang zhjimin\n","color:#f5fafd;background-color:#002d4d;font-size:32px;");
 
+// FIXME: hack on mdui
 var $=mdui.JQ;
 var topNav=$("#top-nav");
 topNav.find("a").each((i,el) => {
-  if(el.getAttribute("data-href")==location.pathname){
+  var href=el.getAttribute("href");
+  el.setAttribute("data-href",href);
+  el.setAttribute("href","#content");
+  if(href==location.pathname){
     el.classList.add("mdui-tab-active");
   }
 });
-// FIXME: hack on mdui
 new mdui.Tab(topNav);
 topNav.find("a").each((i,el) => {
   el.setAttribute("href",
