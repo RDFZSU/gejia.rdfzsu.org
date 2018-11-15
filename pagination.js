94d1269b094edee89ddc6065db7ced2e0fad07d4
Alan?Liang <a@keeer.net>
2018-11-15 14:41:43 +0800

    Change cdn to KEEER server
    12356

--- pagination.js
+++ pagination.js
@@ -6,16 +6,25 @@ function paginationTo(el){
       var isin=((rect.top<window.innerHeight)&&(rect.bottom>0));
       if(isin&&!images[i].isin){
         images[i].setAttribute("src",images[i].getAttribute("data-src"));
-        images[i].style.height="";
       }
       if(!isin&&images[i].isin){
         images[i].setAttribute("src",nullsrc);
-        images[i].style.height=rect.height+"px";
       }
       images[i].isin=isin;
     }
   };
+  var onresize=function(){
+    for (var i = 0; i < images.length; i++) {
+      var width=window.getComputedStyle(images[i]).width;
+      var height=parseFloat(width)*9/16;
+      images[i].style.height=height+"px";
+    }
+  }
   window.addEventListener("scroll",onscroll);
+  window.addEventListener("click",onscroll);
+  window.addEventListener("click",onresize);
+  window.addEventListener("resize",onresize);
   var nullsrc="";
+  onresize();
   onscroll();
 }
